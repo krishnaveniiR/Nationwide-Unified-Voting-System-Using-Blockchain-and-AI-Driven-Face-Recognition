55ba51b3dc8d7f05a565e8924f3134a251264613
krishnaveniiR <154951491+krishnaveniiR@users.noreply.github.com>
2025-05-09 20:03:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,625 @@
+from flask import Flask
+from flask import Flask, render_template, Response, redirect, request, session, abort, url_for
+from camera import VideoCamera
+from camera2 import VideoCamera2
+from datetime import datetime
+from datetime import date
+import datetime
+import random
+from random import seed
+from random import randint
+import cv2
+import numpy as np
+import threading
+import os
+import time
+import shutil
+import imagehash
+import PIL.Image
+from PIL import Image
+from PIL import ImageTk
+import urllib.request
+import urllib.parse
+from urllib.request import urlopen
+import webbrowser
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  charset="utf8",
+  database="voting_finger_bc"
+)
+
+
+app = Flask(__name__)
+##session key
+app.secret_key = 'abcdef'
+@app.route('/',methods=['POST','GET'])
+def index():
+    cnt=0
+    act=""
+    msg=""
+    vid=request.args.get("vid")
+    ff=open("det.txt","w")
+    ff.write("1")
+    ff.close()
+
+    ff1=open("photo.txt","w")
+    ff1.write("1")
+    ff1.close()
+
+    ff11=open("img.txt","w")
+    ff11.write("1")
+    ff11.close()
+
+    return render_template('index.html',msg=msg,act=act)
+
+
+@app.route('/index2',methods=['POST','GET'])
+def index2():
+    cnt=0
+    act=""
+    msg=""
+    vid=request.args.get("vid")
+    ff=open("det.txt","w")
+    ff.write("1")
+    ff.close()
+
+    ff1=open("photo.txt","w")
+    ff1.write("1")
+    ff1.close()
+
+    ff11=open("img.txt","w")
+    ff11.write("1")
+    ff11.close()
+
+    return render_template('index2.html',msg=msg,act=act,vid=vid)
+
+def getImagesAndLabels(path):
+
+    
+    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+    faceSamples=[]
+    ids = []
+
+    for imagePath in imagePaths:
+
+        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        img_numpy = np.array(PIL_img,'uint8')
+
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+
+    return faceSamples,ids
+
+
+@app.route('/add_photo',methods=['POST','GET'])
+def add_photo():
+    vid=request.args.get("vid")
+    ff1=open("photo.txt","w")
+    ff1.write("2")
+    ff1.close()
+
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM vt_voter where id=%s",(vid,))
+    value = cursor.fetchone()
+    name=value[3]
+
+    ff2=open("mask.txt","w")
+    ff2.write("face")
+    ff2.close()
+    
+    if request.method=='GET':
+        
+        ff=open("user.txt","w")
+        ff.write(name)
+        ff.close()
+
+        ff=open("user1.txt","w")
+        ff.write(vid)
+        ff.close()
+    
+    if request.method=='POST':
+        vid=request.form['vid']
+        fimg="v"+vid+".jpg"
+        cursor = mydb.cursor()
+
+        cursor.execute('delete from vt_face WHERE vid = %s', (vid, ))
+        mydb.commit()
+
+        ff=open("det.txt","r")
+        v=ff.read()
+        ff.close()
+        vv=int(v)
+        v1=vv-1
+        #vface1=vid+"_"+str(v1)+".jpg"
+        vface1="User."+vid+"."+str(v1)+".jpg"
+        i=2
+        while i<vv:
+            
+            cursor.execute("SELECT max(id)+1 FROM vt_face")
+            maxid = cursor.fetchone()[0]
+            if maxid is None:
+                maxid=1
+            #vface=vid+"_"+str(i)+".jpg"
+            vface="User."+vid+"."+str(i)+".jpg"
+            sql = "INSERT INTO vt_face(id, vid, vface) VALUES (%s, %s, %s)"
+            val = (maxid, vid, vface)
+            print(val)
+            cursor.execute(sql,val)
+            mydb.commit()
+            i+=1
+
+        shutil.copy('faces/f1.jpg', 'static/photo/'+vface1)
+        ##########
+        
+        ##Training face
+        # Path for face image database
+        path = 'dataset'
+
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+        # function to get the images and label data
+        
+
+        print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+        faces,ids = getImagesAndLabels(path)
+        recognizer.train(faces, np.array(ids))
+
+        # Save the model into trainer/trainer.yml
+        recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+        # Print the numer of faces trained and end program
+        print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+
+
+
+
+        return redirect(url_for('view_photo',vid=vid,act='success'))
+        
+
+    return render_template('add_photo.html', vid=vid)
+
+
+
+
+
+###Preprocessing
+@app.route('/view_photo',methods=['POST','GET'])
+def view_photo():
+    ff1=open("photo.txt","w")
+    ff1.write("1")
+    ff1.close()
+    vid=""
+    value=[]
+    if request.method=='GET':
+        vid = request.args.get('vid')
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM vt_face where vid=%s",(vid, ))
+        value = mycursor.fetchall()
+
+    if request.method=='POST':
+        print("Training")
+        vid=request.form['vid']
+        cursor = mydb.cursor()
+        cursor.execute("SELECT * FROM vt_face where vid=%s",(vid, ))
+        dt = cursor.fetchall()
+        for rs in dt:
+            ##Preprocess
+            path="static/frame/"+rs[2]
+            path2="static/process1/"+rs[2]
+            mm2 = PIL.Image.open(path).convert('L')
+            rz = mm2.resize((200,200), PIL.Image.ANTIALIAS)
+            rz.save(path2)
+            
+            '''img = cv2.imread(path2) 
+            dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
+            path3="static/process2/"+rs[2]
+            cv2.imwrite(path3, dst)'''
+            ######
+            img = cv2.imread(path2)
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+            # noise removal
+            kernel = np.ones((3,3),np.uint8)
+            opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+            # sure background area
+            sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+            # Finding sure foreground area
+            dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+            ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+            # Finding unknown region
+            sure_fg = np.uint8(sure_fg)
+            segment = cv2.subtract(sure_bg,sure_fg)
+            img = Image.fromarray(img)
+            segment = Image.fromarray(segment)
+            path3="static/process2/"+rs[2]
+            segment.save(path3)
+            
+            #####
+            image = cv2.imread(path2)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            edged = cv2.Canny(gray, 50, 100)
+            image = Image.fromarray(image)
+            edged = Image.fromarray(edged)
+            path4="static/process3/"+rs[2]
+            edged.save(path4)
+            ##
+            #shutil.copy('static/images/11.png', 'static/process4/'+rs[2])
+       
+        return redirect(url_for('view_photo1',vid=vid))
+        
+    return render_template('view_photo.html', result=value,vid=vid)
+
+###Segmentation using RNN
+def crfrnn_segmenter(model_def_file, model_file, gpu_device, inputs):
+    
+    assert os.path.isfile(model_def_file), "File {} is missing".format(model_def_file)
+    assert os.path.isfile(model_file), ("File {} is missing. Please download it using "
+                                        "./download_trained_model.sh").format(model_file)
+
+    if gpu_device >= 0:
+        caffe.set_device(gpu_device)
+        caffe.set_mode_gpu()
+    else:
+        caffe.set_mode_cpu()
+
+    net = caffe.Net(model_def_file, model_file, caffe.TEST)
+
+    num_images = len(inputs)
+    num_channels = inputs[0].shape[2]
+    assert num_channels == 3, "Unexpected channel count. A 3-channel RGB image is exptected."
+    
+    caffe_in = np.zeros((num_images, num_channels, _MAX_DIM, _MAX_DIM), dtype=np.float32)
+    for ix, in_ in enumerate(inputs):
+        caffe_in[ix] = in_.transpose((2, 0, 1))
+
+    start_time = time.time()
+    out = net.forward_all(**{net.inputs[0]: caffe_in})
+    end_time = time.time()
+
+    print("Time taken to run the network: {:.4f} seconds".format(end_time - start_time))
+    predictions = out[net.outputs[0]]
+
+    return predictions[0].argmax(axis=0).astype(np.uint8)
+
+
+def run_crfrnn(input_file, output_file, gpu_device):
+    """ Runs the CRF-RNN segmentation on the given RGB image and saves the segmentation mask.
+    Args:
+        input_file: Input RGB image file (e.g. in JPEG format)
+        output_file: Path to save the resulting segmentation in PNG format
+        gpu_device: ID of the GPU device. If using the CPU, set this to -1
+    """
+
+    input_image = 255 * caffe.io.load_image(input_file)
+    input_image = resize_image(input_image)
+
+    image = PILImage.fromarray(np.uint8(input_image))
+    image = np.array(image)
+
+    palette = get_palette(256)
+    #PIL reads image in the form of RGB, while cv2 reads image in the form of BGR, mean_vec = [R,G,B] 
+    mean_vec = np.array([123.68, 116.779, 103.939], dtype=np.float32)
+    mean_vec = mean_vec.reshape(1, 1, 3)
+
+    # Rearrange channels to form BGR
+    im = image[:, :, ::-1]
+    # Subtract mean
+    im = im - mean_vec
+
+    # Pad as necessary
+    cur_h, cur_w, cur_c = im.shape
+    pad_h = _MAX_DIM - cur_h
+    pad_w = _MAX_DIM - cur_w
+    im = np.pad(im, pad_width=((0, pad_h), (0, pad_w), (0, 0)), mode='constant', constant_values=0)
+
+    # Get predictions
+    segmentation = crfrnn_segmenter(_MODEL_DEF_FILE, _MODEL_FILE, gpu_device, [im])
+    segmentation = segmentation[0:cur_h, 0:cur_w]
+
+    output_im = PILImage.fromarray(segmentation)
+    output_im.putpalette(palette)
+    output_im.save(output_file)
+###CNN Classification
+def CNN_process(self):
+        
+        train_data_preprocess = ImageDataGenerator(
+                rescale = 1./255,
+                shear_range = 0.2,
+                zoom_range = 0.2,
+                horizontal_flip = True)
+
+        test_data_preprocess = (1./255)
+
+        train = train_data_preprocess.flow_from_directory(
+                'dataset/training',
+                target_size = (128,128),
+                batch_size = 32,
+                class_mode = 'binary')
+
+        test = train_data_preprocess.flow_from_directory(
+                'dataset/test',
+                target_size = (128,128),
+                batch_size = 32,
+                class_mode = 'binary')
+
+        ## Initialize the Convolutional Neural Net
+
+        # Initialising the CNN
+        cnn = Sequential()
+
+        # Step 1 - Convolution
+        # Step 2 - Pooling
+        cnn.add(Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation = 'relu'))
+        cnn.add(MaxPooling2D(pool_size = (2, 2)))
+
+        # Adding a second convolutional layer
+        cnn.add(Conv2D(32, (3, 3), activation = 'relu'))
+        cnn.add(MaxPooling2D(pool_size = (2, 2)))
+
+        # Step 3 - Flattening
+        cnn.add(Flatten())
+
+        # Step 4 - Full connection
+        cnn.add(Dense(units = 128, activation = 'relu'))
+        cnn.add(Dense(units = 1, activation = 'sigmoid'))
+
+        # Compiling the CNN
+        cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+        history = cnn.fit_generator(train,
+                                 steps_per_epoch = 250,
+                                 epochs = 25,
+                                 validation_data = test,
+                                 validation_steps = 2000)
+
+        plt.plot(history.history['acc'])
+        plt.plot(history.history['val_acc'])
+        plt.title('Model Accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model Loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+
+        test_image = image.load_img('\\dataset\\', target_size=(128,128))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = cnn.predict(test_image)
+        print(result)
+
+        if result[0][0] == 1:
+                print('feature extracted and classified')
+        else:
+                print('none')
+                
+@app.route('/view_photo1',methods=['POST','GET'])
+def view_photo1():
+    vid=""
+    value=[]
+    if request.method=='GET':
+        vid = request.args.get('vid')
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM vt_face where vid=%s",(vid, ))
+        value = mycursor.fetchall()
+    return render_template('view_photo1.html', result=value,vid=vid)
+
+@app.route('/view_photo2',methods=['POST','GET'])
+def view_photo2():
+    vid=""
+    value=[]
+    if request.method=='GET':
+        vid = request.args.get('vid')
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM vt_face where vid=%s",(vid, ))
+        value = mycursor.fetchall()
+    return render_template('view_photo2.html', result=value,vid=vid)    
+
+@app.route('/view_photo3',methods=['POST','GET'])
+def view_photo3():
+    vid=""
+    value=[]
+    if request.method=='GET':
+        vid = request.args.get('vid')
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM vt_face where vid=%s",(vid, ))
+        value = mycursor.fetchall()
+    return render_template('view_photo3.html', result=value,vid=vid)
+
+@app.route('/view_photo4',methods=['POST','GET'])
+def view_photo4():
+    vid=""
+    value=[]
+    if request.method=='GET':
+        vid = request.args.get('vid')
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM vt_face where vid=%s",(vid, ))
+        value = mycursor.fetchall()
+    return render_template('view_photo4.html', result=value,vid=vid)
+
+@app.route('/message',methods=['POST','GET'])
+def message():
+    vid=""
+    name=""
+    if request.method=='GET':
+        vid = request.args.get('vid')
+        mycursor = mydb.cursor()
+        
+    return render_template('message.html',vid=vid,name=name)
+
+
+@app.route('/verify_face',methods=['POST','GET'])
+def verify_face():
+    msg=""
+    ss=""
+    uname=""
+    act=""
+    vid=request.args.get("vid")
+    if request.method=='GET':
+        act = request.args.get('act')
+        
+    #if 'username' in session:
+    #    uname = session['username']
+    ff2=open("un.txt","r")
+    uname=ff2.read()
+    ff2.close()
+
+
+
+    now = datetime.datetime.now()
+    rdate=now.strftime("%d-%m-%Y")
+        
+       
+                
+    return render_template('verify_face.html',msg=msg,vid=vid)
+
+@app.route('/process',methods=['POST','GET'])
+def process():
+    msg=""
+    ss=""
+    st=""
+    chk=""
+    vid=request.args.get("vid")
+    uname=""
+    act=""
+    det=""
+    # (0, 1) is N
+    SCALE = 2.2666 # the scale is chosen to be 1 m = 2.266666666 pixels
+    MIN_LENGTH = 150 # pixels
+
+    if request.method=='GET':
+        act = request.args.get('act')
+        
+    #if 'username' in session:
+    #    uname = session['username']
+    ff2=open("un.txt","r")
+    uname=ff2.read()
+    ff2.close()
+    #print("uname="+uname)
+    #shutil.copy('static/faces/f1.jpg', 'static/f1.jpg')
+
+    ff3=open("img.txt","r")
+    mcnt=ff3.read()
+    ff3.close()
+
+    ff2=open("facest.txt","r")
+    fst=ff2.read()
+    ff2.close()
+
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM vt_voter where id=%s",(fst, ))
+    value = cursor.fetchall()
+
+    n=0
+    for value1 in value:
+        n+=1
+    #try:
+    
+    mcnt1=int(mcnt)
+
+    if n>0:
+        #vid=value[0]
+        chk="yes"
+        msg="Face Match"
+        st="1"       
+        cursor.execute("update vt_voter set status=1 where id=%s",(fst,))
+        mydb.commit()
+        cursor.execute("update vt_booth set status=1,voterid=%s where id=1",(fst,))
+        mydb.commit()
+    '''if mcnt1>=2:
+        msg="Face Detected"
+        cutoff=10
+        act="1"
+        cursor.execute('SELECT * FROM vt_face')
+        dt = cursor.fetchall()
+        for rr in dt:
+            print("static/frame/"+rr[2])
+            hash0 = imagehash.average_hash(Image.open("static/frame/"+rr[2])) 
+            hash1 = imagehash.average_hash(Image.open("static/faces/f1.jpg"))
+            cc1=hash0 - hash1
+            print("cc="+str(cc1))
+            if cc1<=cutoff:
+                vid=rr[1]
+                chk="yes"
+                msg="Face Match"
+                st="1"                  
+                break
+            else:
+                
+                msg="Unknown"'''
+    #except:
+    #    print("excep")
+
+    if chk=="yes":
+        st="1"
+        
+    if mcnt1>=2:
+        msg="Detected!"
+    else:
+        msg="Face not Detected"
+        
+    return render_template('process.html',msg=msg,act=act,mcnt1=mcnt1,det=det,vid=vid,st=st)
+
+
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it is there
+    #session.pop('username', None)
+    return redirect(url_for('index'))
+#########
+def gen2(camera):
+    
+    while True:
+        frame = camera.get_frame()
+        
+        
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    
+@app.route('/video_feed2')       
+def video_feed2():
+    return Response(gen2(VideoCamera2()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+def gen(camera):
+    
+    while True:
+        frame = camera.get_frame()
+        
+        
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    
+@app.route('/video_feed')
+        
+
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=5000)
